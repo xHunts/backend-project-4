aee2d14bd7201ccc1a34162653964d23180f4cb5
zanaalma <zanaalma1@gmail.com>
2020-01-30 16:34:10 +0300

    add event model

---
+++ app/models/event.js
@@ -0,0 +1,38 @@
+//import mongoose
+const mongoose = require('mongoose')
+const eventSchema = new mongoose.Schema ({
+    name: {
+    type: String,
+    required: true
+    },
+
+    place: {
+    type:String,
+    required: true
+    },
+    link:{
+        type:String,
+        required: true
+    },
+    date: {
+    type: Date,
+    required: true
+    },
+
+    registers:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }],
+
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+
+    }, {
+      timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema) 
